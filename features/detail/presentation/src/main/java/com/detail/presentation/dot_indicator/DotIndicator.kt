1b04d9e3db209b9c632cf775507b17bd40778f9b
Jonathan Eduardo Meriño Bolivar <jmerino1204@gmail.com>
2024-10-06 11:23:33 -0500

    DotIndicator (#3)

---
+++ features/detail/presentation/src/main/java/com/detail/presentation/dot_indicator/DotIndicator.kt
@@ -0,0 +1,71 @@
+package com.detail.presentation.dot_indicator
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DotIndicator(
+    count: Int,
+    pagerState: PagerState,
+    modifier: Modifier = Modifier,
+    spacing: Dp = 10.dp
+) {
+
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(spacing),
+            modifier = modifier
+                .height(10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            repeat(count) {
+                Box(
+                    modifier = Modifier
+                        .size(10.dp)
+                        .background(
+                            color = Color(0xFFD9D9D9),
+                            shape = CircleShape
+                        )
+
+                )
+            }
+        }
+
+        Box(
+            Modifier
+                .size(10.dp)
+                .background(
+                    color = Color(0xFF3483FA),
+                    shape = CircleShape
+                )
+        )
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+private fun DotIndicatorPreview() {
+    val state = rememberPagerState { 0 }
+    DotIndicator(count = 5, pagerState = state)
+}
